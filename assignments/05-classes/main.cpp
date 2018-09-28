524bec09e4e7f49d0cf44450f3b0d24a7f193f51
allen lim <allenman23@yahoo.com>
2018-09-27 19:38:49 -0500

    added output to main

--- assignments/05-classes/main.cpp
+++ assignments/05-classes/main.cpp
@@ -1,6 +1,12 @@
 #include "invoice_detail.h"
+#include<iostream>
 
 int main()
 {
+	Invoice_Detail invoice_ex1(10, 10);
+	invoice_ex1.get_extended_cost();
+	std::cout << "Created a class for Invoice Detail\nCreated an instance: invoice_ex1\ninvoice_ex1.get_extended_cost(): "
+		<< invoice_ex1.get_extended_cost();
+
 	return 0;
 }
