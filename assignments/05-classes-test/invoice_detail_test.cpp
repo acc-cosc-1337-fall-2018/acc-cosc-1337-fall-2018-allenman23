12ccd7d970f6213e3803b9edc6e8556d5759e004
allen lim <allenman23@yahoo.com>
2018-10-18 17:40:50 -0500

    midterm

--- assignments/05-classes-test/invoice_detail_test.cpp
+++ assignments/05-classes-test/invoice_detail_test.cpp
@@ -1,55 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-//#include "invoice_detail.h"
-#include "invoice.h"
-#include "invoice_utility.h"
-#include "invoice_progress.h"
 #include "midterm.h"
 
-TEST_CASE("Test cases for get extended cost")
-{
-	Invoice_Detail invoice_example1(10, 10);
-	REQUIRE(invoice_example1.get_extended_cost() == 100);
-}
-
-TEST_CASE("Test cases for invoice get_total")
-{
-	Invoice invoice1;
-	invoice1.add_invoice_detail(Invoice_Detail(10, 10));
-	invoice1.add_invoice_detail(Invoice_Detail(5, 5));
-	invoice1.add_invoice_detail(Invoice_Detail(100, 2));
-	REQUIRE(invoice1.get_total() == 325);
-
-}
-
-TEST_CASE("Test invoice operator overloading")
-{
-	Invoice invoice1;
-	invoice1.add_invoice_detail(Invoice_Detail(10, 10));
-	invoice1.add_invoice_detail(Invoice_Detail(5, 5));
-	invoice1.add_invoice_detail(Invoice_Detail(100, 2));
-
-	Invoice invoice2;
-	invoice2.add_invoice_detail(Invoice_Detail(100, 2));
-
-	Invoice result = invoice1 + invoice2;
-	REQUIRE(result.get_total() == 525);
-
-}
-
-TEST_CASE("Test Invoice Utility get total")
-{
-	Invoice_Utility inv(25);
-	inv.add_invoice_detail(Invoice_Detail(100, 1));
-	REQUIRE(inv.get_total() == 125);
-}
-
-TEST_CASE("Test Invoice Progress get_total")
-{
-	Invoice_Progress inv(250);
-	inv.add_invoice_detail(Invoice_Detail(100, 1));
-	REQUIRE(inv.get_total() == 150);
-}
 
 TEST_CASE("Test get_acgt")
 {
--- assignments/05-classes/main.cpp
+++ assignments/05-classes/main.cpp
@@ -1,54 +1,17 @@
-//#include "invoice_detail.h"
-#include "invoice.h"
 #include<iostream>
-#include"invoice_utility.h"
-#include"invoice_progress.h"
-#include <memory>
 #include"midterm.h"
-/*
-class Shape
-{
-public:
-	virtual void draw() { std::cout << "Shape\n"; }
-};
 
-class Line : public Shape
-{
-public:
-	void draw() { std::cout << "Line\n"; }
-};
-
-class Circle : public Shape
-{
-public:
-	void draw() { std::cout << "Circle\n"; }
-
-};
-*/
 int main()
 {
-//	std::unique_ptr<Shape> line = std::make_unique<Line>();
-//	line->draw();
-/*	std::vector<std::unique_ptr<Shape>> shapes;
-	shapes.push_back(std::make_unique<Line>());
-	shapes.push_back(std::make_unique<Circle>());
-
-	for (auto& shape : shapes)
-	{
-		shape->draw();
-	}
-*/
 	std::vector<int> dna_example;
 	dna_example = get_acgt("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC");
 	for (auto i : dna_example)
 		std::cout << i << "\n";
 
-	std::vector<int> many_grades{ 60, 75, 77, 88, 90, 50, 100 };
+	std::vector<int> many_grades{ 60, 75, 77, 88, 90, 50, 100, 65, 71, 5, 95, 77, 80, 89, 97, 94, 76, 30, 85, 78};
 	get_grades(many_grades);
 
 	Receipt rec1;
-//	std::cin >> rec1;
-//	std::cout << rec1;
 	std::vector<Receipt> receipts;
 	Receipt rec2(10), rec3(20), rec4(30);
 	receipts.push_back(rec2);
@@ -85,8 +48,10 @@ int main()
 	//the compiler does not let you alter the string,
 	//the "const" allows read only
 
+	//OUTPUT:
 	//Draw Line
 	//Draw Circle
+
 	//the classes Line and Circle inherit from the base class Shape
 	//the virtual operation allows for references of Shape to use Line and Circle functions 
 	return 0;
