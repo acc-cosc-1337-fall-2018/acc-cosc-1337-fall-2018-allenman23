68498c7798e3a93e376fd7459fd75964f96c778a
allen lim <allenman23@yahoo.com>
2018-10-30 18:11:07 -0500

    assigment update

--- assignments/05-classes-test/invoice_detail_test.cpp
+++ assignments/05-classes-test/invoice_detail_test.cpp
@@ -1,23 +1,49 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "invoice.h"
+#include "invoice_utility.h"
+#include "invoice_progress.h"
 #include "midterm.h"
 
-
-TEST_CASE("Test get_acgt")
+TEST_CASE("Test cases for get extended cost")
 {
-	REQUIRE(get_acgt("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC") == std::vector<int>{20, 12, 17, 21});
+	Invoice_Detail invoice_example1(10, 10);
+	REQUIRE(invoice_example1.get_extended_cost() == 100);
 }
 
-TEST_CASE("Test for total amount of Receipt")
+TEST_CASE("Test cases for invoice get_total")
 {
-	Receipt test_receipt(10);
-	REQUIRE(test_receipt.get_total() == 13);
+	Invoice invoice1;
+	invoice1.add_invoice_detail(Invoice_Detail(10, 10));
+	invoice1.add_invoice_detail(Invoice_Detail(5, 5));
+	invoice1.add_invoice_detail(Invoice_Detail(100, 2));
+	REQUIRE(invoice1.get_total() == 325);
 }
 
-TEST_CASE("Test the overloaded += operator")
+TEST_CASE("Test invoice operator overloading")
 {
-	Receipt test1(10), test2(10);
-	test1 += test2;
-	REQUIRE(test1.get_total() == 26);
+	Invoice invoice1;
+	invoice1.add_invoice_detail(Invoice_Detail(10, 10));
+	invoice1.add_invoice_detail(Invoice_Detail(5, 5));
+	invoice1.add_invoice_detail(Invoice_Detail(100, 2));
 
+	Invoice invoice2;
+	invoice2.add_invoice_detail(Invoice_Detail(100, 2));
+
+	Invoice result = invoice1 + invoice2;
+	REQUIRE(result.get_total() == 525);
+}
+
+TEST_CASE("Test Invoice Utility get total")
+{
+	Invoice_Utility inv(25);
+	inv.add_invoice_detail(Invoice_Detail(100, 1));
+	REQUIRE(inv.get_total() == 125);
+}
+
+TEST_CASE("Test Invoice Progress get_total")
+{
+	Invoice_Progress inv(250);
+	inv.add_invoice_detail(Invoice_Detail(100, 1));
+	REQUIRE(inv.get_total() == 150);
 }
--- assignments/05-classes/main.cpp
+++ assignments/05-classes/main.cpp
@@ -1,58 +1,40 @@
 #include<iostream>
-#include"midterm.h"
+#include "invoice.h"
+#include"invoice_utility.h"
+#include"invoice_progress.h"
+#include <memory>
+
+class Shape
+{
+public:
+	virtual void draw() { std::cout << "Shape\n"; }
+};
+
+class Line : public Shape
+{
+public:
+	void draw() { std::cout << "Line\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "Circle\n"; }
+
+};
 
 int main()
 {
-	std::vector<int> dna_example;
-	dna_example = get_acgt("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC");
-	for (auto i : dna_example)
-		std::cout << i << "\n";
+	//	std::unique_ptr<Shape> line = std::make_unique<Line>();
+	//	line->draw();
+	std::vector<std::unique_ptr<Shape>> shapes;
+	shapes.push_back(std::make_unique<Line>());
+	shapes.push_back(std::make_unique<Circle>());
 
-	std::vector<int> many_grades{ 60, 75, 77, 88, 90, 50, 100, 65, 71, 5, 95, 77, 80, 89, 97, 94, 76, 30, 85, 78};
-	get_grades(many_grades);
+	for (auto& shape : shapes)
+	{
+		shape->draw();
+	}
 
-	Receipt rec1;
-	std::vector<Receipt> receipts;
-	Receipt rec2(10), rec3(20), rec4(30);
-	receipts.push_back(rec2);
-	std::cout << rec2;
-	receipts.push_back(rec3);
-	std::cout << rec3;
-	receipts.push_back(rec4);
-	std::cout << rec4;
-	Receipt result;
-	for (auto i : receipts)
-		result += i;
-	std::cout << result;
-
-	Line l;
-	Shape& shape1 = l;
-	shape1.draw();
-	Circle c;
-	Shape& shape2 = c;
-	shape2.draw();
-	l.draw();
-	c.draw();
-
-	std::string name{ "Allen Lim" };
-	std::cout << name << "\n";
-	reverse_string_by_reference(name);
-	std::cout << name << "\n";
-	//string changes because it is passed into the function by reference
-
-	name = "Allen Lim";
-	std::cout << name << "\n" << reverse_string_by_value(name) << "\n";
-	//the returned value is not the same variable (same address of original string)
-	//it is a copy of the original string, reversed, then returned
-
-	//the compiler does not let you alter the string,
-	//the "const" allows read only
-
-	//OUTPUT:
-	//Draw Line
-	//Draw Circle
-
-	//the classes Line and Circle inherit from the base class Shape
-	//the virtual operation allows for references of Shape to use Line and Circle functions 
 	return 0;
 }
