a952bf6dbeabb4fdf163eb3d64b1d30e7f243d80
allen lim <allenman23@yahoo.com>
2018-11-29 19:17:18 -0600

    constructors & destructors

--- 01-basics/basics.cpp
+++ 01-basics/basics.cpp
@@ -16,3 +16,49 @@ void pass_by_val_by_ref(int val, int& ref)
 	val = 99;
 	ref = 99;
 }
+
+void const_pass_by_val_by_ref(int val, const int & ref)
+{
+	val = 99;
+	//ref = 99; ref can't be modified
+}
+
+void pass_by_pointer(int * ptr)
+{
+	//ptr: we're using the address
+	*ptr = 99;//*ptr: get the fvalue of the address pointed to
+}
+
+void initialize_vector_of_ints()
+{
+}
+
+void initialize_vector_of_strings()
+{
+}
+
+MyClass::MyClass()
+{
+	std::cout << "Constructor...\n";
+}
+
+MyClass::MyClass(MyClass & my_class)
+{
+	ptr_num = new int(*my_class.ptr_num);
+}
+
+void MyClass::set_ptr_num(int val)
+{
+	*ptr_num = val;
+}
+
+MyClass & MyClass::operator=(MyClass other)
+{
+	ptr_num = new int(*other.ptr_num);
+	return *this;
+}
+
+MyClass::~MyClass()
+{
+	std::cout << "Num: " <<num << "\nPtr num: " << *ptr_num << "\nAddress at Ptr num: " << ptr_num << "\nDestructor...\n";
+}
--- 01-basics/main.cpp
+++ 01-basics/main.cpp
@@ -1,11 +1,30 @@
 ﻿#include<iostream>
 #include "basics.h"
+#include<memory>
 
 using std::cout;
 
 int main()
 {
-	cout << sum_numbers(5, 5);
+//	cout << sum_numbers(5, 5);
+/*	int num = 10;
+	pass_by_pointer(&num);
+	cout << num;
+*/
 
+/*	MyClass c;
+	MyClass a = c;
+	a.set_ptr_num(1000);
+*/
+/*
+	MyClass* ptr_class = new MyClass();
+	ptr_class->set_ptr_num(50);
+	delete ptr_class;
+	ptr_class = nullptr;
+*/
+	std::unique_ptr<MyClass> ptr_class = std::make_unique<MyClass>();
+	ptr_class->set_ptr_num(50);
+	MyClass ptr_class2 = *ptr_class;
+	ptr_class2.set_ptr_num(100);
 	return 0;
 }
